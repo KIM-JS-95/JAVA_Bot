869122a34d065bf3e2670beeecb6f5fc8e0fcb55
KIM-JS-95 <baugh248730@gmail.com>
2021-02-26 23:56:33 +0900

    2021-02-26

---
+++ src/test/java/ScreenCaptureTest.java
@@ -0,0 +1,26 @@
+
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ScreenCaptureTest {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Robot robot = new Robot();
+            String format = "jpg";
+            String fileName = "FullScreenshot." + format;
+
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+
+            System.out.println("A full screenshot saved!");
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
