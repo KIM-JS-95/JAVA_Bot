e547201a57f044fa4fc310c81d15eb48bab0ddc9
KIM-JS-95 <baugh248730@gmail.com>
2021-02-27 01:27:41 +0900

    2021-02-27

---
+++ README.md
@@ -0,0 +1,8 @@
+# JAVA_Robot class + openCV
+
+## 알비온 온라인 낚시 매크로 제작
+### 탬플릿 매칭
+
+
+### REFERENCE
+https://docs.opencv.org/3.4/de/da9/tutorial_template_matching.html
--- src/test/java/ScreenCaptureTest.java
+++ src/test/java/ScreenCaptureTest.java
@@ -16,6 +16,8 @@ public class ScreenCaptureTest {
 
             Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
             BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+
+
             ImageIO.write(screenFullImage, format, new File(fileName));
 
             System.out.println("A full screenshot saved!");
